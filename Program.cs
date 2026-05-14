595f3437ef32c125d6e19c1a921dc8f94eaaa544
Dmitry <playpplay8@gmail.com>
2026-05-14 13:44:32 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Jerry");
+ABC();
+Thread.Sleep(500);
+ABC();
+Thread.Sleep(500);
+ABC();
+Thread.Sleep(500);
+
+A();
+
+// функция
+void ABC()
+{
+Console.WriteLine("A");
+Console.WriteLine("B");
+Console.WriteLine("C");
+}
+
+void A()
+{
+B();
+C();
+}
+
+void B()
+{
+Console.WriteLine("BBBBB");
+}
+
+void C()
+{
+Console.WriteLine("CCCCC");
+}
+
+
+void NeIspolzuem(){}
